8adbd85f3ee20f2ecbab60f5d641c13a574aaf4b
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-09-24 13:47:55 +0300

    boxes endpoint ready to use in this stage

--- app/Http/Controllers/Api/v1/BoxController.php
+++ app/Http/Controllers/Api/v1/BoxController.php
@@ -4,36 +4,43 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Box\BoxStoreRequest;
+use App\Http\Requests\Api\Box\BoxUpdateRequest;
 use App\Http\Resources\Api\Box\BoxShowResource;
 use App\Models\Box;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BoxController extends Controller
 {
     public function index(): JsonResponse
     {
-        $box = Box::select('id', 'title')
-            ->where('user_id', \Auth::id())
-            ->get();
-        return response()->json($box);
+        try {
+            $box = Box::select('id', 'title')
+                ->where('user_id', \Auth::id())
+                ->get();
+            return response()->json($box);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
     }
 
     public function store(BoxStoreRequest $request): JsonResponse
     {
         $validated = $request->validated();
 
-        $userId = \Auth::id();
-
-        $box = Box::create([
-            'title' => $validated['title'],
-            'user_id' => \Auth::id(),
-        ]);
+        try {
+            $box = Box::create([
+                'title' => $validated['title'],
+                'user_id' => \Auth::id(),
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
 
         return response()->json([
             'id' => $box->id,
             'title' => $box->title
-        ]);
+        ], Response::HTTP_CREATED);
     }
 
     public function show(Box $box): JsonResponse
@@ -41,26 +48,26 @@ class BoxController extends Controller
         return response()->json(new BoxShowResource($box));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(BoxUpdateRequest $request, Box $box): JsonResponse
     {
-        //
+        $validated = $request->validated();
+        try {
+            $box = $box->update($validated);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
+
+        return response()->json($box, Response::HTTP_ACCEPTED);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(Box $box): JsonResponse
     {
-        //
+        try {
+            $box->delete();
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
